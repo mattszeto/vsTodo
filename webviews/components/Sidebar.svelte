970b35fe9ca4fc6339dbc4ba2ef001f551b38b4a
mattszeto <ms8288@gmail.com>
2021-01-13 15:41:08 -0800

    some basic svelte added for sidebar ui and ux

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -1,10 +1,32 @@
 <script lang="ts">
+    import HelloWorld from "./HelloWorld.svelte";
+
+    let todos: Array<{ text: string; completed: boolean }> = [];
+    let text = "";
 </script>
 
 <style>
-    div {
-        color: pink;
+    .complete {
+        text-decoration: line-through;
     }
 </style>
 
-<div>Hello</div>
+<form
+    on:submit|preventDefault={(e) => {
+        todos = [{ text, completed: false }, ...todos];
+        text = '';
+    }}>
+    <input bind:value={text} />
+</form>
+
+<ul>
+    {#each todos as todo (todo.text)}
+        <li
+            class:complete={todo.completed}
+            on:click={() => {
+                todo.completed = !todo.completed;
+            }}>
+            {todo.text}
+        </li>
+    {/each}
+</ul>
